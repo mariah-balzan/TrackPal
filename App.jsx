0185e99629264e50a39a51c4e0925720df345f6b
Mariah Balzan <mariah.balzan.20@um.edu.mt>
2023-02-21 13:26:52 +0100

    Draft of main features

--- App.jsx
+++ App.jsx
@@ -1,20 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { DarkTheme, DefaultTheme, NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Onboarding from './screens/Onboarding';
+// import LoginScreen from './Screens/LoginScreen';
+// import LoginRegister from './Screens/LoginRegister';
+// import HomeScreen from './Screens/HomeScreen';
+// import SignupScreen from './Screens/SignupScreen';
+import { EventRegister } from 'react-native-event-listeners';
+import { React, useState, useEffect }  from 'react';
+// import theme from './theme/theme'
+// import themeContext from './theme/themeContext';
+
+//Create instance of StackNavigator
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  // const [isFirstLaunch, setIsFirstLaunch] = useState(null);
+
+  // useEffect(() => {
+  //   AsyncStorage.getItem('alreadyLaunched').then(value => {
+  //     if(value == null){
+  //       AsyncStorage.setItem('alreadyLaunched', 'true');
+  //       setIsFirstLaunch(true);
+  //     }else{
+  //       setIsFirstLaunch(false);
+  //     }
+  //   })
+  // }, [])
+
+  //   if(isFirstLaunch == null){
+  //       return null;
+  // }else if(isFirstLaunch == true){
+    const [darkMode, setDarkMode] = useState(false);
+    useEffect(() => {
+      const listener = EventRegister.addEventListener('ChangeTheme', (data) => {
+        setDarkMode(data)
+      })
+      return () => {
+        EventRegister.removeAllListeners(listener)
+      }
+    }, [darkMode])
+    return(
+      // <themeContext.Provider value = {darkMode === true ? theme.dark : theme.light}>
+        // <NavigationContainer theme={darkMode === true ? DarkTheme : DefaultTheme}>
+        <NavigationContainer>
+            {/* Removes the top title and expands screen area */}
+          <Stack.Navigator screenOptions={{headerShown: false}}>
+            <Stack.Screen
+            name = "Onboarding"
+            component = {Onboarding}
+            />
+            {/* <Stack.Screen
+            name = "LoginRegister"
+            component = {LoginRegister}
+            />
+            <Stack.Screen
+            name = "Register"
+            component = {SignupScreen}
+            />
+            <Stack.Screen
+            name = "Login"
+            component = {LoginScreen}
+            />
+            <Stack.Screen
+            name = "Home"
+            component = {HomeScreen}
+            /> */}
+          </Stack.Navigator>
+        </NavigationContainer>
+      // </themeContext.Provider>
+      
+        );
+      // }else{
+      //   return <LoginRegister/>
+      // }
+  }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
---
+++ screens/Onboarding.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import {View, Text, Button, StyleSheet, Image, Dimensions} from "react-native";
+import Onboarding from 'react-native-onboarding-swiper';
+import AppLoading from 'expo-app-loading';
+import { useFonts } from "expo-font";
+
+const { width } = Dimensions.get('screen');
+const { height } = Dimensions.get('screen');
+
+const Skip = ({...props}) => (
+    <Button
+        title='Skip'
+        color='#000000' 
+        {...props}
+        />
+);
+const Next = ({...props}) => (
+    <Button
+        title='Next'
+        color='#000000' 
+        {...props}
+        />
+);
+
+export default function OnBoarding ({navigation}){
+    let[fontsLoaded] = useFonts({
+        'Comfortaa' : require('../assets/fonts/Comfortaa/Comfortaa-VariableFont_wght.ttf'),
+    })
+
+if(!fontsLoaded){
+    return <AppLoading/>
+}
+
+
+    return(
+        <Onboarding
+        SkipButtonComponent={Skip}
+        NextButtonComponent={Next}
+        //skip can have .replace to remove from stack if skip is clicked
+        // onSkip={() => navigation.navigate("LoginRegister")}
+        // onDone={() => navigation.navigate("LoginRegister")}
+            pages= {[
+                {
+                // backgroundColor: '#F4F7F8',
+                backgroundColor: "#F4F7F8",
+                image: <Image style={{width:320, height:320}}source= {require("../assets/onboarding/screen1.png")}/>,
+                title: <Text style= {{color:'#219EBC', fontSize:'43', fontFamily:'Comfortaa', textAlign: 'center'}}>TrackPal</Text>,
+                subtitle: <Text style= {{color:'#219EBC', fontSize:'25', fontFamily:'Comfortaa', paddingTop: '10%', textAlign:'center', padding:'5%'}}>Your virtual assistant</Text>,
+                },
+                {
+                    backgroundColor: '#8ECAE6',
+                    image: <Image style={{resizeMode: "stretch",width: width * 0.9, height:height/2.8}}source= {require("../assets/onboarding/screen2.png")} />,
+                    title: <Text style= {{color:'#F4F7F8', fontSize:'43', fontFamily:'Comfortaa', textAlign: 'center'}}>Security, Mobility and Assitance</Text>,
+                    subtitle:  <Text style= {{color:'#F4F7F8', fontSize:'25', fontFamily:'Comfortaa', paddingTop:'10%',textAlign:'center', padding: '7%'}}>Feel comfortable</Text>,
+                },
+                {
+                    backgroundColor: '#FFDD89',
+                    image: <Image style={{resizeMode: "stretch",width: width * 1, height:height/2.4}}source= {require("../assets/onboarding/screen3.png")} />,
+                    title: <Text style= {{color:'#023047', fontSize:'43', fontFamily:'Comfortaa', textAlign:'center'}}>Create an account now</Text>,
+                    subtitle:  <Text style= {{color:'#023047', fontSize:'25', fontFamily:'Comfortaa', paddingTop:'10%',textAlign:'center', padding:'7%'}}>Feel comfortable</Text>,
+                },
+            ]}
+            />
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+  
